0f6218b9e45cca63f115e08a6f66110257b61b8c
Lea ANDRE <aleaandre@gmail.com>
2025-01-15 16:03:12 +0100

    [AF-408] feat (user register): display the home page with register and login buttons, linked to register page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
-  { path: '', component: HomeComponent }];
+  { path: '', component: HomeComponent },
+  { title: 'Register', path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,9 +3,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+const materialModule = [
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, RegisterComponent],
@@ -13,7 +28,8 @@ import { RegisterComponent } from './auth/register/register.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    LayoutModule,
+    ...materialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
