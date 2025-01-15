fd81191d9ad446ea752e1bea786d88afbf810432
Lea ANDRE <aleaandre@gmail.com>
2025-01-15 20:01:20 +0100

    [AF-408] feat (user register): add header menu, which is hide in home page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  public title: string = 'front';
+  public showHeader: boolean = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (val.url == '/') {
+          this.showHeader = false;
+        } else {
+          this.showHeader = true;
+        }
+      }
+    });
+  }
 }
