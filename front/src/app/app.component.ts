6db342dac04841c9bc3a1b57395e860a859b3cbd
Lea ANDRE <aleaandre@gmail.com>
2025-01-16 10:48:38 +0100

    [AF-408] feat (user register): handle handset for layout behaviour

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { combineLatest, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +12,28 @@ export class AppComponent {
   public title: string = 'front';
   public showHeader: boolean = true;
 
-  constructor(private router: Router) {
-    router.events.subscribe((val) => {
-      if (val instanceof NavigationEnd) {
-        if (val.url == '/') {
+  constructor(private router: Router, private responsive: BreakpointObserver) {
+    const url$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event: any) => event.url) // Capture l'URL actuelle
+    );
+
+    const isPhonePortrait$ = this.responsive
+      .observe([Breakpoints.HandsetPortrait])
+      .pipe(map((result) => result.matches)); // True si téléphone en portrait
+
+    // Combine les deux streams
+    combineLatest([url$, isPhonePortrait$]).subscribe(
+      ([url, isPhonePortrait]) => {
+        if (
+          url === '/' ||
+          (isPhonePortrait && (url === '/register' || url === '/login'))
+        ) {
           this.showHeader = false;
         } else {
           this.showHeader = true;
         }
       }
-    });
+    );
   }
 }
