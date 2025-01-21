d57d6cefa7307343f6832cd765ccbe3e8c840d42
Lea ANDRE <aleaandre@gmail.com>
2025-01-21 17:13:15 +0100

    [AF-440] feat (FRONTEND user details): userService to get user with findById

--- front/src/app/components/user-details/user-details.component.ts
+++ front/src/app/components/user-details/user-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/interfaces/user.interface';
 import { SessionService } from 'src/app/services/session.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -8,24 +10,30 @@ import { SessionService } from 'src/app/services/session.service';
   styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent implements OnInit {
-  public form: FormGroup;
+  public form = this.fb.group({
+    userName: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(20)],
+    ],
+    email: ['', [Validators.required, Validators.email]],
+  });
 
-  constructor(private sessionService: SessionService, private fb: FormBuilder) {
-    this.form = this.fb.group({
-      userName: [
-        this.sessionService.sessionInformation?.userName,
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(20),
-        ],
-      ],
-      email: [
-        this.sessionService.sessionInformation?.email,
-        [Validators.required, Validators.email],
-      ],
-    });
+  constructor(
+    private sessionService: SessionService,
+    private fb: FormBuilder,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService
+      .getById(this.sessionService.sessionInformation!.id.toString())
+      .subscribe((user: User) => this.initForm(user));
   }
 
-  ngOnInit(): void {}
+  public initForm(user: User) {
+    this.form.patchValue({
+      userName: user.userName,
+      email: user.email,
+    });
+  }
 }
