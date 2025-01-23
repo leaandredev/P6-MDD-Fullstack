dd1d475d6b2277857a560cc9e499ce7532e8fdcc
Lea ANDRE <aleaandre@gmail.com>
2025-01-23 16:01:21 +0100

    [AF-456] feat (BACK topics): add entity, repository, dto and mapper for topics

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/TopicDto.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.mddapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopicDto {
+    private Long id;
+
+    @NonNull
+    @Size(max = 40)
+    private String title;
+
+    @Size(max = 255)
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
