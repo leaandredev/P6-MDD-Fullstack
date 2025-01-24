adeb1cb6011423392a8a155425365aa30d562158
Lea ANDRE <aleaandre@gmail.com>
2025-01-24 12:02:49 +0100

    [AF-462] feat (FRONT topics): disable subscription button for already subscribed topics

--- front/src/app/features/feed/components/topics/topics.component.ts
+++ front/src/app/features/feed/components/topics/topics.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TopicService } from '../../services/topic.service';
+import { UserService } from 'src/app/services/user.service';
+import { SessionService } from 'src/app/services/session.service';
+import { User } from 'src/app/interfaces/user.interface';
 
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
   styleUrls: ['./topics.component.scss'],
 })
-export class TopicsComponent {
+export class TopicsComponent implements OnInit {
   public topics$ = this.topicService.all();
+  public subscriptions: number[] = [];
 
-  constructor(private topicService: TopicService) {}
+  constructor(
+    private topicService: TopicService,
+    private userService: UserService,
+    private sessionService: SessionService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService
+      .getById(this.sessionService.sessionInformation!.id.toString())
+      .subscribe((user: User) => {
+        this.subscriptions = user.subscriptions || [];
+      });
+  }
 }
