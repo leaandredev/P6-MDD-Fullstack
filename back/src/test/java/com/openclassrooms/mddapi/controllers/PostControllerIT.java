e89350662e6fb0e11a65eb60ee110f60e564a9d2
Lea ANDRE <aleaandre@gmail.com>
2025-01-31 13:59:46 +0100

    remove unused import

--- back/src/test/java/com/openclassrooms/mddapi/controllers/PostControllerIT.java
+++ back/src/test/java/com/openclassrooms/mddapi/controllers/PostControllerIT.java
@@ -2,7 +2,6 @@ package com.openclassrooms.mddapi.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
--- back/src/test/java/com/openclassrooms/mddapi/models/UserTest.java
+++ back/src/test/java/com/openclassrooms/mddapi/models/UserTest.java
@@ -8,7 +8,6 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
