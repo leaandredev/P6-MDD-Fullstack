d17add36e1511d270cb8b59b6491c9122361ac7a
Lea ANDRE <aleaandre@gmail.com>
2025-01-31 16:38:11 +0100

    [AF-372] feat (FRONT comments): send the comment from details post page

--- front/src/app/features/post/components/post-detail/post-detail.component.ts
+++ front/src/app/features/post/components/post-detail/post-detail.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { Comment } from 'src/app/core/interfaces/comment.interface';
 import { PostResponse } from 'src/app/core/interfaces/postResponse.interface';
+import { CommentService } from 'src/app/core/services/comment.service';
 import { PostService } from 'src/app/core/services/post.service';
+import { SessionService } from 'src/app/core/services/session.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -11,8 +16,17 @@ import { PostService } from 'src/app/core/services/post.service';
 export class PostDetailComponent implements OnInit {
   public postId: string;
   public post: PostResponse | undefined;
+  public form: FormGroup | undefined;
+  public onError: boolean = false;
 
-  constructor(private postService: PostService, private route: ActivatedRoute) {
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private sessionService: SessionService,
+    private commentService: CommentService,
+    private matSnackBar: MatSnackBar
+  ) {
     this.postId = this.route.snapshot.paramMap.get('id')!;
   }
 
@@ -20,9 +34,36 @@ export class PostDetailComponent implements OnInit {
     this.postService.getById(this.postId).subscribe((post: PostResponse) => {
       this.post = post;
     });
+    this.form = this.fb.group({
+      content: ['', Validators.required],
+    });
   }
 
   public back() {
     window.history.back();
+  }
+
+  public addComment() {
+    if (this.form && this.form.valid) {
+      const commentRequest = this.form.value as Comment;
+      commentRequest.postId = Number(this.postId);
+      commentRequest.userId = this.sessionService.sessionInformation!.id;
+      this.commentService.create(commentRequest).subscribe({
+        next: () => {
+          this.matSnackBar.open(
+            'Votre commentaire a bien été ajouté.',
+            'Close',
+            {
+              duration: 2000,
+            }
+          );
+          this.form?.reset();
+          this.form?.get('content')?.setErrors(null);
+        },
+        error: () => {
+          this.onError = true;
+        },
+      });
+    }
   }
 }
